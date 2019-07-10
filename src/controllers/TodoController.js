295f0c50cd6a85508a346b5123fccc7dd062ceca
Mohamed Jamal OUAZANI TOUHAMI <medjamal.ouazani@gmail.com>
2019-07-10 03:02:36 +0100

    Complited CRUD functionality of todos

--- src/controllers/TodoController.js
+++ src/controllers/TodoController.js
@@ -16,6 +16,28 @@ todoController.get = async (request, response, next) => {
     }
 };
 
+todoController.getOne = async  (request, response, next) => {
+    const id = request.params.id;
+
+    try {
+        const todo = await Todo.findById({ _id: id });
+        
+        if(todo === null) return response.status(404).send({
+            success: false,
+            message: 'Todo does not exist in database!'
+        });
+
+        return response.status(200).send({
+            success: true,
+            todo: todo
+        });
+
+    } catch (error) {
+        console.log('Error get one todo: Todo Controller with mongoose');
+        next(error);
+    }
+}
+
 todoController.create = async (request, response, next) => {
     console.log('Create Todo');
 
@@ -37,5 +59,59 @@ todoController.create = async (request, response, next) => {
         next(error);
     }
 };
+
+todoController.update = async (request, response, next) => {
+    const id = request.params.id;
+    const { title } = request.body;
+
+    // Check if Todo does not exist in the db and retuen false with message ..
+
+    // if(todo === null) return response.status(404).send({
+    //     success: false,
+    //     message: 'Todo does not exist in database!'
+    // });
+
+    try {
+        const todo = await Todo.updateOne(
+            { _id: id },
+            { title: title }
+        );
+
+        if(todo.n === 0) {
+            return response.status(404).send({ success: false, message: 'Todo does not exist!' });
+        }
+
+        return response.status(201).send({
+            success: true,
+            todo: todo
+        });
+    } catch (error) {
+        console.log('Error update: Todo Controller with mongoose');
+        next(error);
+    }
+}
+
+todoController.destroy = async (request, response, next) => {
+    const id = request.params.id;
+
+    try {
+        const result = await Todo.deleteOne({ _id: id });
+        if(result.deletedCount === 0) {
+            response.status(404);
+            return response.send({
+                success: false,
+                message: 'Todo not found!'
+            });
+        }
+    } catch (error) {
+        console.log('Error delete: Todo Controller with mongoose');
+        next(error);
+    }
+    
+    return response.send({
+        success: true,
+        message: 'Todo deleted'
+    });
+}
 
 module.exports = todoController;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const todoController = require('../controllers/TodoController')
 
 router.get('/todos', todoController.get);
+router.get('/todo/:id', todoController.getOne);
 router.post('/todo', todoController.create);
+router.put('/todo/:id', todoController.update);
+router.delete('/todo/:id', todoController.destroy);
 
 module.exports = router;
